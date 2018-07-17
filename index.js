0584cfafabc316f3abc5701292f62006c693c150
Shangni Hu <shangnihu@gmail.com>
2018-07-17 03:34:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Shangni Hu';
 
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
